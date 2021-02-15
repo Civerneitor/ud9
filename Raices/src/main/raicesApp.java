1f5cd1e36ebf7479a58ed0e3c4faa6c04187569b
Sergio <sergio_castilla_13@hotmail.com>
2021-02-15 18:00:42 +0100

    ejercicio 4

---
+++ Raices/src/main/raicesApp.java
@@ -0,0 +1,14 @@
+package main;
+import models.Raices;
+public class raicesApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a=3;
+    	double b=9;
+    	double c=8;
+    	Raices e=new Raices(a,b,c);
+    	e.calcular(); 
+	}
+
+}
